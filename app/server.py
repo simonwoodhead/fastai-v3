0f8ce8202da14e4fb8311693c67d55299ea6e009
Simon Woodhead <simon.woodhead@gmail.com>
2019-05-03 15:21:00 +0100

    Changed bears to sloths...

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1X_HLrUqRThmcw8cE4_9dNmCNLpNInJsB'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['hoffman', 'maned', 'pygmy']
 path = Path(__file__).parent
 
 app = Starlette()
